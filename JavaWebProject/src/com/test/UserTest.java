49786b4a060fd490bcae482fe90311965c04ee42
jaylan <1419349524@qq.com>
2023-10-17 23:57:34 +0800

    test:新增用户 单元测试

--- JavaWebProject/src/com/test/UserTest.java
+++ JavaWebProject/src/com/test/UserTest.java
@@ -1,4 +1,12 @@
 package com.test;
 
+import com.dto.UserDto;
+
 public class UserTest {
+    public static void main(String[] args) {
+        //判断用户名是否存在
+//        System.out.println(UserDto.isExist("admin"));
+//        新增用户
+        System.out.println(UserDto.addUser("huaji1","huaji233"));
+    }
 }
