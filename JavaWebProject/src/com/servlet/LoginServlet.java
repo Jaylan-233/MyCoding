a23afee6e3c2455133e79dc5bf3841c2482d3769
jaylan <1419349524@qq.com>
2023-10-17 23:59:08 +0800

    feat:登录servlet

---
+++ JavaWebProject/src/com/servlet/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.servlet;
+
+import com.dto.UserDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/userLogin")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+
+        String password = req.getParameter("password");
+        if (UserDto.Login(username,password))
+        {
+
+            req.getRequestDispatcher("html/LoginSuccess.html").forward(req,resp);
+        }else
+        {
+
+            req.getRequestDispatcher("html/LoginFailure.html").forward(req,resp);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
