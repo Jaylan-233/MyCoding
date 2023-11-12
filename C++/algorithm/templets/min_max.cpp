1b8ba3d3d3252b16d54b389b401628a6c1b4ff19
jaylan <1419349524@qq.com>
2023-11-12 23:15:29 +0800

    Feat:max和min函数的使用

---
+++ C++/algorithm/templets/min_max.cpp
@@ -0,0 +1,13 @@
+// 求两个数中的最大值 和最小值
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 比较两个值
+    int a=99;
+    int b=88;
+    cout<<"a和b中最小值是:"<<min(a,b)<<endl;
+    cout<<"a和b中最大值是:"<<max(a,b)<<endl;
+    // 只能对两个数进行比较 
+    return 0;
+}
