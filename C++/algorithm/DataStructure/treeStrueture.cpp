1c5c8fbcbda8c90f8d5f6be81b308b95a3ee8eba
jaylan <1419349524@qq.com>
2023-11-26 23:38:49 +0800

    Feat:二叉树结构体定义

---
+++ C++/algorithm/DataStructure/treeStrueture.cpp
@@ -0,0 +1,11 @@
+// 二叉树结构体定义
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    // TreeNode类的构造函数 用来创建新的TreeNode对象 并初始化 的 val=s 左右节点都为NULL
+    // 函数名称(参数类型 参数名称):代码块 {}
+    // 写这个是为了更好的创建节点
+};
