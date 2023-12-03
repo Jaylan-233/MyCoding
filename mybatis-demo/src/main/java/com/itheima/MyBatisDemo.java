f804a8b87f47787e6d40e06da0d1f2bf552ce739
Jaylan <1419349524@qq.com>
2023-12-03 12:24:23 +0800

    Chore:删除多余的类

--- mybatis-demo/src/main/java/com/itheima/MyBatisDemo.java
+++ mybatis-demo/src/main/java/com/itheima/MyBatisDemo.java
@@ -12,23 +12,25 @@ import java.io.InputStream;
 import java.util.List;
 
 /**
- * Mybatis 快速入门代码
+ * Mybatis 代理开发
  */
 public class MyBatisDemo {
 
     public static void main(String[] args) throws IOException {
 
-        //1. 加载mybatis的核心配置文件，获取 SqlSessionFactory
+        //1. 加载mybatis的核心配置文件mybatis-config.xml，获取 SqlSessionFactory
         String resource = "mybatis-config.xml";
+        //1.1读取配置文件信息并
         InputStream inputStream = Resources.getResourceAsStream(resource);
+        //1.2根据配置文件创建  sqlSessionFactory 对象 只创建一次
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
-        //2. 获取SqlSession对象，用它来执行sql
+        //2. 通过 sqlSessionFactory 对象 获取SqlSession对象，用它来执行sql
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //3. 执行sql
+        //参数是命名空间.语句id(Mapper中的)
         List<User> users = sqlSession.selectList("test.selectAll");
         System.out.println(users);
-        //4. 释放资源
+        //4. 执行完成了之后记得释放资源释放资源
         sqlSession.close();
 
     }
