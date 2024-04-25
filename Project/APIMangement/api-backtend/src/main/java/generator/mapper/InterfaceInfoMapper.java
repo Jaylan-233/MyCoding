4a7853533a126033d152b123a362541a424dc95b
Jaylan <1419349524@qq.com>
2024-04-25 16:54:42 +0800

    Feat:使用MybatisX生成Interface业务代码

---
+++ Project/APIMangement/api-backtend/src/main/java/generator/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Jaylan
+* @description 针对表【interface_info(接口信息表)】的数据库操作Mapper
+* @createDate 2024-04-25 16:52:03
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ Project/APIMangement/api-backtend/src/main/java/generator/service/InterfaceInfoService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.InterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Jaylan
+* @description 针对表【interface_info(接口信息表)】的数据库操作Service
+* @createDate 2024-04-25 16:52:03
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+}
