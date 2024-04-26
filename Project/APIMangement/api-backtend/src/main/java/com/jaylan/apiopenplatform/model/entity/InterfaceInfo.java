8a3119e4ac216e077dafc04a6cb5caa6cae587e5
Jaylan <1419349524@qq.com>
2024-04-26 12:43:06 +0800

    Chore :isDelete增加@TableLogic注解

--- Project/APIMangement/api-backtend/src/main/java/com/jaylan/apiopenplatform/model/entity/InterfaceInfo.java
+++ Project/APIMangement/api-backtend/src/main/java/com/jaylan/apiopenplatform/model/entity/InterfaceInfo.java
@@ -1,9 +1,7 @@
 package com.jaylan.apiopenplatform.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
@@ -73,7 +71,11 @@ public class InterfaceInfo implements Serializable {
 
     /**
      * 是否删除(0-未删除 1-已删除)
+     * 逻辑删除
+     *  查询的时候自动过滤
+     *
      */
+    @TableLogic
     private Integer isDelete;
 
     @TableField(exist = false)
