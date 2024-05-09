e173122a9b1cbe223c09b8fa6538d3a9254e183e
Jaylan <1419349524@qq.com>
2024-05-09 12:30:58 +0800

    迁移项目

---
+++ Spring/SpringDemo/JDBCDemo/src/main/java/org/jaylan/pojo/Account.java
@@ -0,0 +1,31 @@
+package org.jaylan.pojo;
+
+public class Account {
+    private  int id;
+    private  String accountName;
+    private  double balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ Spring/SpringDemo/JDBCDemo/src/main/java/org/jaylan/pojo/Test.java
@@ -0,0 +1,14 @@
+package org.jaylan.pojo;
+
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext=new ClassPathXmlApplicationContext("spring.xml")
+                //TODO: xml 实现durid 配置
+
+
+    }
+}
